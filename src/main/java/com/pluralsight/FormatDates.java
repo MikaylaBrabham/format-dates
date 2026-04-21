92358dc30121acb8bc406042ab73bd852cdb058a
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-21 15:26:02 -0500

    added date and time sentence output

---
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+import java.time.LocalDateTime;
+
+public class FormatDates {
+    public static void main(String[] args) {
+
+        //Get the local date and time right now
+        LocalDateTime dateTime1 = LocalDateTime.now();
+        System.out.println("Today is: " + dateTime1);
+
+    }
+}
