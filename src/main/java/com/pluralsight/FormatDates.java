00df25d14ade9c6fd767087fa896932067a5b00b
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-21 16:00:59 -0500

    Started formatting like different ones

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -7,14 +7,31 @@ public class FormatDates {
 
         //Get the local date and time right now
         LocalDateTime dateTimeNow = LocalDateTime.now();
-        System.out.println("Today is: " + dateTimeNow);
+        System.out.println("Today's current time and date without any formatting is: " + dateTimeNow);
 
+
+
+        //get the local date and time in mm/dd/yyyy format
+        DateTimeFormatter formatDateTime1 = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        String formattedDateTime1 = dateTimeNow.format(formatDateTime1);
+        System.out.println("Today is: " + formattedDateTime1);
+
+        //get the local date and time in yyyy-mm-yy format
+        DateTimeFormatter formatDateTime2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String formattedDateTime2 = dateTimeNow.format(formatDateTime2);
+        System.out.println("Today is: " + formattedDateTime2);
+
+        //get the local date and time in dayofmonth dd, yyyy
+
+        //get the local date and time in dayofmonth, partialmonth dd, yyyy time in civilian hours xx:xx
+
+        //format the current day, date, and time rightnow
         DateTimeFormatter formatDateTime =
-            DateTimeFormatter.ofPattern("E, MMM dd, yyyy HH:mm:ss");
+            DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy HH:mm:ss");
 
-        String formattedDateTime = dateTimeNow.format(formatDateTime);
-
-        System.out.println("Today is: " + formattedDateTime);
+        //display the current day and time with my formatting
+        String formattedDateTime5 = dateTimeNow.format(formatDateTime);
+        System.out.println("Today is: " + formattedDateTime5);
 
     }
 }
