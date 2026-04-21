e26b97c486378d0fc6bc87fd87c2c219c86c3bb3
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-21 15:43:47 -0500

    added comments

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -1,12 +1,20 @@
 package com.pluralsight;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class FormatDates {
     public static void main(String[] args) {
 
         //Get the local date and time right now
-        LocalDateTime dateTime1 = LocalDateTime.now();
-        System.out.println("Today is: " + dateTime1);
+        LocalDateTime dateTimeNow = LocalDateTime.now();
+        System.out.println("Today is: " + dateTimeNow);
+
+        DateTimeFormatter formatDateTime =
+            DateTimeFormatter.ofPattern("E, MMM dd, yyyy HH:mm:ss");
+
+        String formattedDateTime = dateTimeNow.format(formatDateTime);
+
+        System.out.println("Today is: " + formattedDateTime);
 
     }
 }
