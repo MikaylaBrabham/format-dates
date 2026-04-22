d6f0181cc3af6928c4766702a80b1e07bcd3e7ee
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-22 12:44:40 -0500

    added imports again with local date now - added 2 formatting dates

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -7,15 +7,32 @@ public class FormatDates {
 
         //Get the local date and time right now
         LocalDateTime dateTime1 = LocalDateTime.now();
+        //display the unformatted date and time now
         System.out.println("Today is: " + dateTime1);
 
         //Create a formatter for the date and time for mm/dd/yyyy
         DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("MM/dd/yyyy");
         String formattedDate1 = dateTime1.format(formatter1);
+        //display the formatted date and time
+        System.out.println("Today is: " + formattedDate1);
 
         //create a formatter for the date and time for yyyy-mm-dd
         DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         String formattedDate2 = dateTime1.format(formatter2);
+        //display the formatted date and time
+        System.out.println("Today is: " + formattedDate2);
+
+        //create a formatter for the date and time for full weekday dd, yyyy
+        DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("EEEE dd, yyyy");
+        String formattedDate3 = dateTime1.format(formatter3);
+        //display the formatted date and time
+        System.out.println("Today is: " + formattedDate3);
+
+        // create a formatter for the date and time for full weekday, month dd, 2026 hh:mm
+        DateTimeFormatter formatter4 = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy hh:mm");
+        String formattedDate4 = dateTime1.format(formatter4);
+        // display the formatted date and time
+        System.out.println("Today is: " + formattedDate4);
 
     }
 }
